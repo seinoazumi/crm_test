426716388af84c8558486c41dadb4283b39e1b21
Azumi Seino <49363119+seinoazumi@users.noreply.github.com>
2020-05-05 12:03:16 +0900

    コントローラーの作成

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -2,4 +2,23 @@ class FacilitiesController < ApplicationController
   def index
     @all_adress = Facility.all
   end
+
+  def new
+    @facility = Facility.new
+  end
+  
+  def create
+    @facility = Facility.new(facility_params)
+    if @facility.save
+      redirect_to facilities_path, notice: t("page.facilities.flash.create.notice")
+    else
+      flash.now[:alart] = t("page.facilities.flash.create.alert")
+      render new_facility_path(@facility)
+    end
+  end
+
+private
+  def facility_params
+    params.require(:facility).permit(:name, :postal_code, :prefecture, :adress1, :adress2, :building, :tel, :fax, :url, :descrioption)
+  end
 end
